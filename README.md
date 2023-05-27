345e0802bf6233c30a28c1a7927521afe62ad2f7
Italo Contreras <icontrerasp@uni.pe>
2023-05-27 01:11:13 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-test
+# Project
+
+A code exam for a job position using react,node and aws services (lambda, s3,dynamo db and api gateway)
+
+## Tabla de Contenidos
+
+- [Requirements](#requirements)
+- [Architecture](#architecture)
+- [Instalation](#instalation)
+- [Use](#use)
+- [Evidences](#evidences)
+- [Contribution](#contribution)
+
+
+## Requirements
+
+aws account, git, node and npm
+
+## Architecture
+
+![architecture_1](./src/img/architecture_1.png)
+
+## Instalation
+
+-git clone (url)
+
+-s3 implementation in aws console
+
+-api gateway implementation in aws console
+
+-lambda implementation in aws console
+
+-dynamo db implementation in aws console
+
+-npm install
+
+-npm build 
+
+-aws s3 sync build/ s3://(bucket)
+
+## Use
+
+##### React
+
+some react code
+
+![react_1](./src/img/react_1.png)
+
+![react_2](./src/img/react_2.png)
+
+##### Lambda
+
+![lambda_1](./src/img/lambda_1.png)
+
+##### Api Gateway
+
+![apigateway_1](./src/img/apigateway_1.png)
+
+##### S3
+
+files
+
+
+![s3_2](./src/img/s3_2.PNG)
+
+permissions
+
+![s3_1](./src/img/s3_1.png)
+
+redirect
+
+![s3_3](./src/img/s3_3.png)
+
+##### Dynamo
+
+![dynamo_1](./src/img/dynamo_1.png)
+
+## Evidences
+
+AWS
+
+http://clients-hosting-code-test.s3-website-us-east-1.amazonaws.com/
+
+![aws](./src/img/aws.png)
+
+Local
+
+![localhost](./src/img/localhost.png)
+
+## Contribution
+
+A yml could be added where the aws services are deployed
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,96 +1,80 @@
-import { Box, Table, TableRow, TableCell, TableHead, Typography, TableBody, styled, Button} from '@mui/material'
+import { Box, Table, TableRow, TableCell, TableHead, Typography, TableBody, styled, Button } from '@mui/material'
 import { useEffect, useState } from 'react';
-
 import axios from 'axios';
 
 const Component = styled(Box)({
-    width: '80%',
-    margin: '50px auto',
-    '& > h4': {
-      marginBottom: '20px'
-    },
-    '& > div > table > thead': {
-      backgroundColor: '#000'
-    },
-    '& > div > table > thead > tr > th': {
-      color: '#FFFFFF'
-    }
+  width: '80%',
+  margin: '50px auto',
+  '& > h4': {
+    marginBottom: '20px'
+  },
+  '& > div > table > thead': {
+    backgroundColor: '#FFB347'
+  },
+  '& > div > table > thead > tr > th': {
+    color: '#FFFFFF'
+  }
 });
 
-const defaultObj = [{
-    id:101,
-    name:"Code test",
-    email:"icontrerasp@uni.pe",
-    phone:986392442,
-    salary:22000,
-    age:28
-}]
 
 
 const Clients = () => {
+  const [clients, setClients] = useState([]);
 
-    // const [users,setUsers] = useState(defaultObj);
-    const [clients,setClients] = useState([]);
+  const API_URL = 'https://j9luw8noe9.execute-api.us-east-2.amazonaws.com/Dev';
 
-    const API_URL = 'https://j9luw8noe9.execute-api.us-east-2.amazonaws.com/Dev';
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        const parsedData = JSON.parse(response.data.body).Items;
+        // Ordenar los clientes por ID de menor a mayor
+        parsedData.sort((a, b) => a.id - b.id);
+        setClients(parsedData);
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getData();
+  }, [])
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await axios.get(API_URL);
-                setClients(JSON.parse(response.data.body).Items);
-            }catch(error){
-                console.error(error)
+  return (
+    <Component>
+      <Typography variant="h4">Clients</Typography>
+      <Box>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>DNI</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Phone</TableCell>
+              <TableCell>Address</TableCell>
+              <TableCell>Age</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              clients.map(client => (
+                <TableRow key={client.id}>
+                  <TableCell>{client.id}</TableCell>
+                  <TableCell>{client.name}</TableCell>
+                  <TableCell>{client.lastName}</TableCell>
+                  <TableCell>{client.dni}</TableCell>
+                  <TableCell>{client.email}</TableCell>
+                  <TableCell>{client.phone}</TableCell>
+                  <TableCell>{client.address}</TableCell>
+                  <TableCell>{client.age}</TableCell>
+                </TableRow>
+              ))
             }
-            
-
-        }
-        getData();
-    },[]) 
-
-
-    return (
-        // <p>Hello from users</p>
-        <Component>
-            <Typography variant="h4">Clients</Typography>
-            <Box>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Id</TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Last Name</TableCell>
-                            <TableCell>DNI</TableCell>
-                            <TableCell>Email</TableCell>
-                            <TableCell>Phone</TableCell>
-                            <TableCell>Address</TableCell>
-                            <TableCell>Age</TableCell>
-                            {/* <TableCell>Remove Entry</TableCell> */}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                            {
-                                clients.map(client => (
-                                    <TableRow key={client.id}>
-                                        <TableCell>{client.id}</TableCell>
-                                        <TableCell>{client.name}</TableCell>
-                                        <TableCell>{client.lastName}</TableCell>
-                                        <TableCell>{client.dni}</TableCell>
-                                        <TableCell>{client.email}</TableCell>
-                                        <TableCell>{client.phone}</TableCell>
-                                        <TableCell>{client.address}</TableCell>
-                                        <TableCell>{client.age}</TableCell>
-                                        {/* <TableCell><Button variant='contained' color='error' onClick={() => removeEntry(user.id)}>Remove</Button></TableCell> */}
-                                    </TableRow>
-                                ))
-                            }
-
-                    </TableBody>
-
-                </Table>
-            </Box>
-        </Component>
-    )
+          </TableBody>
+        </Table>
+      </Box>
+    </Component>
+  )
 }
 
 export default Clients;
